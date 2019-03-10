d8f4372fff119febdf0cc78c11223fc6264abffe
Ryan Fuhrman <rsfuhrman10@gmail.com>
2019-03-10 15:51:53 -0400

    Updated fields autocompleted by boilerplate

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Ryan Fuhrman`,
+    description: `The personal website and portfolio of Ryan Fuhrman.`,
+    author: `@ryanfuhrman`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
