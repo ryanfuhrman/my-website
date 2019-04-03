0514df779bfe47978890135edbfbc23a4ebe8c35
Ryan Fuhrman <rsfuhrman10@gmail.com>
2019-04-03 12:50:35 -0400

    Tried to convert Goodreads API return to nodes but has proven difficult since the data returned is crap

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,61 +1,68 @@
-const axios = require("axios")
-const crypto = require("crypto")
-const convertToJson = require("xml-js")
+// const axios = require("axios")
+// const crypto = require("crypto")
+// const convertToJson = require("xml-js")
 
-exports.sourceNodes = async ({ actions }) => {
-  const { createNode } = actions
-  const goodReadsKey = "55fwR0TWKwR7Q9ypEH3Xw"
+// exports.sourceNodes = async ({ actions }) => {
+//   const { createNode } = actions
+//   const goodReadsKey = "55fwR0TWKwR7Q9ypEH3Xw"
 
-  // fetch raw data from the randomuser api
-  const fetchCurrentlyReading = () =>
-    axios({
-      url: `https://www.goodreads.com/review/list/17888634.xml?key=${goodReadsKey}&shelf=currently-reading`,
-    }).then(res => {
-      const bookData = convertToJson.xml2json(res.data, {
-        compact: true,
-        spaces: 4,
-      })
+//   // fetch raw data from the randomuser api
+//   const fetchCurrentlyReading = () =>
+//     axios({
+//       url: `https://www.goodreads.com/review/list/17888634.xml?key=${goodReadsKey}&shelf=currently-reading`,
+//     }).then(res => {
+//       const bookData = convertToJson.xml2json(res.data, {
+//         compact: true,
+//         spaces: 4,
+//       })
 
-      return bookData
-    })
+//       return JSON.parse(bookData)
+//     })
 
-  const res = await fetchCurrentlyReading()
+//   const res = await fetchCurrentlyReading()
 
-  console.log(res.GoodreadsResponse)
-  // map into these results and create nodes
-  // res.GoodReadsResponse.books.map((book, i) => {
-  //   // Create your node object
-  //   const bookNode = {
-  //     // Required fields
-  //     id: `${i}`,
-  //     parent: `__SOURCE__`,
-  //     internal: {
-  //       type: `Book`, // name of the graphQL query --> allRandomUser {}
-  //       // contentDigest will be added just after
-  //       // but it is required
-  //     },
-  //     children: [],
+//   // console.log(res.GoodreadsResponse.books)
+//   // map into these results and create nodes
+//   const book = res.GoodreadsResponse.books
+//   const convertedToArray = Object.keys(book).map(key => {
+//     return [key, book[key]]
+//   })
 
-  //     // Other fields that you want to query with graphQl
-  //     book: {
-  //       title: book.title._text,
-  //       author: book.authors.author.name._text,
-  //       image: book.image_url._text,
-  //       isbn: book.isbn._text,
-  //     },
-  //   }
+//   console.log(convertedToArray)
 
-  //   // Get content digest of node. (Required field)
-  //   const contentDigest = crypto
-  //     .createHash(`md5`)
-  //     .update(JSON.stringify(bookNode))
-  //     .digest(`hex`)
-  //   // add it to bookNode
-  //   bookNode.internal.contentDigest = contentDigest
+//   convertedToArray.map((book, i) => {
+//     // Create your node object
+//     const bookNode = {
+//       // Required fields
+//       id: `${i}`,
+//       parent: `__SOURCE__`,
+//       internal: {
+//         type: `Book`, // name of the graphQL query --> allRandomUser {}
+//         // contentDigest will be added just after
+//         // but it is required
+//       },
+//       children: [],
 
-  //   // Create node with the gatsby createNode() API
-  //   createNode(bookNode)
-  // })
+//       // Other fields that you want to query with graphQl
+//       book: {
+//         title: book.title,
+//         author: book.authors,
+//         image: book.image_url,
+//         isbn: book.isbn,
+//       },
+//     }
 
-  return
-}
+//     // Get content digest of node. (Required field)
+//     const contentDigest = crypto
+//       .createHash(`md5`)
+//       .update(JSON.stringify(bookNode))
+//       .digest(`hex`)
+//     // add it to bookNode
+//     bookNode.internal.contentDigest = contentDigest
+
+//     // Create node with the gatsby createNode() API
+//     createNode(bookNode)
+//   })
+
+//   return
+// }
--- src/components/CurrentlyReading.js
+++ src/components/CurrentlyReading.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react"
-// import axios from "axios"
-// import convertToJson from "xml-js"
+import axios from "axios"
+import convertToJson from "xml-js"
 
 class CurrentlyReading extends Component {
   state = {
@@ -11,31 +11,31 @@ class CurrentlyReading extends Component {
   }
 
   componentDidMount() {
-    // const goodReadsKey = "55fwR0TWKwR7Q9ypEH3Xw"
-    // // const googleKey = "AIzaSyDWUdACh9Aq_mCkbCoDxQalF4DAlXkGjPc"
-    // axios({
-    //   url: `https://cors-anywhere.herokuapp.com/https://www.goodreads.com/review/list/17888634.xml?key=${goodReadsKey}&shelf=currently-reading`,
-    // }).then(res => {
-    //   const bookData = convertToJson.xml2json(res.data, {
-    //     compact: true,
-    //     spaces: 4,
-    //   })
-    //   const bookDataJson = JSON.parse(bookData)
-    //   console.log(bookDataJson.GoodreadsResponse.books)
-    //   const book = bookDataJson.GoodreadsResponse.books.book
-    //   const author = book.authors.author.name._text
-    //   const title = book.title._text
-    //   const image = book.image_url._text
-    //   const isbn = book.isbn._text
-    //   console.log(isbn)
-    //   this.setState({
-    //     author: author,
-    //     title: title,
-    //     image: image,
-    //     isbn: isbn,
-    //   })
-    // })
-    // .then(getBookCover(this.state.isbn))
+    const goodReadsKey = "55fwR0TWKwR7Q9ypEH3Xw"
+    // const googleKey = "AIzaSyDWUdACh9Aq_mCkbCoDxQalF4DAlXkGjPc"
+    axios({
+      url: `https://cors-anywhere.herokuapp.com/https://www.goodreads.com/review/list/17888634.xml?key=${goodReadsKey}&shelf=currently-reading`,
+    }).then(res => {
+      const bookData = convertToJson.xml2json(res.data, {
+        compact: true,
+        spaces: 4,
+      })
+      const bookDataJson = JSON.parse(bookData)
+      console.log(bookDataJson.GoodreadsResponse.books)
+      const book = bookDataJson.GoodreadsResponse.books.book
+      const author = book.authors.author.name._text
+      const title = book.title._text
+      const image = book.image_url._text
+      const isbn = book.isbn._text
+      console.log(isbn)
+      this.setState({
+        author: author,
+        title: title,
+        image: image,
+        isbn: isbn,
+      })
+    })
+    //   .then(getBookCover(this.state.isbn))
     // const getBookCover = function(isbn) {
     //   axios({
     //     url: `https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}&key=${googleKey}`,
