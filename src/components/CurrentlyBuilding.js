6806638ac4c64c300ec3b133eeb20aa36bd06cbe
Ryan Fuhrman <rsfuhrman10@gmail.com>
2019-04-11 13:12:08 -0400

    Created component for displaying projects

---
+++ src/components/CurrentlyBuilding.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+
+class CurrentlyBuilding extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Currently Building</h2>
+        <h3>Check out what I've been working on.</h3>
+      </div>
+    )
+  }
+}
+
+export default CurrentlyBuilding
