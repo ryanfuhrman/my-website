f21d7dc191bfd5fee1b779815825d919bf2d0c75
Ryan Fuhrman <rsfuhrman10@gmail.com>
2019-04-19 15:47:29 -0400

    Converted inline styles to styled-components

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,48 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import styled from "styled-components"
+
+const StyledHeader = styled.header`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-areas: "left right";
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  right: 0px;
+  z-index: 2;
+  padding: 16px;
+  background: rgb(246, 247, 248);
+
+  .left {
+    grid-area: left;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+  }
+  .left h1 {
+    display: flex;
+    align-items: center;
+  }
+  .left h1 link {
+    color: black;
+    text-decoration: none;
+    font-size: 22px;
+  }
+
+  .right {
+    grid-area: right;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+  }
+`
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
+  <StyledHeader>
+    <div className="left">
+      <h1>
         <Link
           to="/"
           style={{
@@ -28,7 +55,12 @@ const Header = ({ siteTitle }) => (
         </Link>
       </h1>
     </div>
-  </header>
+    <div className="right">
+      <a href="mailto:rsfuhrman10@gmail.com">
+        <button>Email</button>
+      </a>
+    </div>
+  </StyledHeader>
 )
 
 Header.propTypes = {
--- src/components/layout.js
+++ src/components/layout.js
@@ -8,9 +8,26 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
+import styled from "styled-components"
 
 import Header from "./header"
 import "./layout.css"
+
+const MainContainer = styled.div`
+  margin: 0 auto;
+  max-width: 960;
+  padding: 0px 1.0875rem 1.45rem;
+  padding-top: 0;
+
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 65px;
+    padding-left: 32px;
+    padding-right: 32px;
+  }
+`
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -26,17 +43,10 @@ const Layout = ({ children }) => (
     render={data => (
       <>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
+        <MainContainer>
           <main>{children}</main>
-          <footer>{new Date().getFullYear()}, No Copyright :)</footer>
-        </div>
+        </MainContainer>
+        <footer>{new Date().getFullYear()}, No Copyright :)</footer>
       </>
     )}
   />
