b26907bc4cddd476ee437613f27f5158ecff6a4c
Ryan Fuhrman <rsfuhrman10@gmail.com>
2019-04-26 14:14:51 -0400

    Removed unncessary wrapper

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,44 +6,42 @@ import { ProjectsContainer } from "./styles/ProjectsStyles"
 
 function Projects() {
   return (
-    <>
-      <Section header="Projects" subheader="What I've been building">
-        <ProjectsContainer>
-          <Project
-            className="twitch-app"
-            owner="ryanfuhrman"
-            name="twitch-app"
-            desc="An app to play with the Twitch API"
-            githubLink="https://github.com/ryanfuhrman/twitch-app"
-            codepenLink=""
-          />
-          <Project
-            className="markdown-viewer"
-            owner="ryanfuhrman"
-            name="markdown-viewer"
-            desc="A markdown playground built with React"
-            githubLink="https://github.com/ryanfuhrman/markdown-viewer"
-            codepenLink=""
-          />
-          <Project
-            className="my-website"
-            owner="ryanfuhrman"
-            name="my-website"
-            desc="Hey, that's this website! 🚀"
-            githubLink="https://github.com/ryanfuhrman/my-website"
-            codepenLink=""
-          />
-          <Project
-            className="calculator"
-            owner="ryanfuhrman"
-            name="calculator"
-            desc="A javascript calculator"
-            githubLink="https://github.com/ryanfuhrman/calculator"
-            codepenLink="https://codepen.io/ryanfuhrman/full/jZzooK"
-          />
-        </ProjectsContainer>
-      </Section>
-    </>
+    <Section header="Projects" subheader="What I've been building">
+      <ProjectsContainer>
+        <Project
+          className="twitch-app"
+          owner="ryanfuhrman"
+          name="twitch-app"
+          desc="An app to play with the Twitch API"
+          githubLink="https://github.com/ryanfuhrman/twitch-app"
+          codepenLink=""
+        />
+        <Project
+          className="markdown-viewer"
+          owner="ryanfuhrman"
+          name="markdown-viewer"
+          desc="A markdown playground built with React"
+          githubLink="https://github.com/ryanfuhrman/markdown-viewer"
+          codepenLink=""
+        />
+        <Project
+          className="my-website"
+          owner="ryanfuhrman"
+          name="my-website"
+          desc="Hey, that's this website! 🚀"
+          githubLink="https://github.com/ryanfuhrman/my-website"
+          codepenLink=""
+        />
+        <Project
+          className="calculator"
+          owner="ryanfuhrman"
+          name="calculator"
+          desc="A javascript calculator"
+          githubLink="https://github.com/ryanfuhrman/calculator"
+          codepenLink="https://codepen.io/ryanfuhrman/full/jZzooK"
+        />
+      </ProjectsContainer>
+    </Section>
   )
 }
 
