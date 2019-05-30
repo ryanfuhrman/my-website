d7b8050c731b7a87a4a093668d454f7fa212b9f8
Ryan Fuhrman <rsfuhrman10@gmail.com>
2019-05-30 18:48:53 -0400

    Updated api key variable name

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -1,2 +1,5 @@
 const fs = require("fs")
-fs.writeFileSync("./.env", `GOODREADS_KEY=${process.env.GOODREADS_KEY}\n`)
+fs.writeFileSync(
+  "./.env",
+  `GATSBY_GOODREADS_KEY=${process.env.GATSBY_GOODREADS_KEY}\n`
+)
--- src/components/Books.js
+++ src/components/Books.js
@@ -10,7 +10,7 @@ class Books extends Component {
   }
 
   componentDidMount() {
-    const goodReadsKey = process.env.GOODREADS_KEY
+    const goodReadsKey = process.env.GATSBY_GOODREADS_KEY
 
     axios({
       url: `https://cors-anywhere.herokuapp.com/https://www.goodreads.com/review/list/17888634.xml?key=${goodReadsKey}&shelf=read&per_page=5&sort=date_added`,
