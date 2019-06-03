b7d4e233c93a38f8def3e1d11c9569295a02cc43
Ryan Fuhrman <rsfuhrman10@gmail.com>
2019-06-03 16:50:11 -0400

    destructured props

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,66 +4,46 @@ import ProjectStyled from './styles/ProjectStyles.js';
 import Github from '../images/github.svg';
 import CodePen from '../images/codepen.svg';
 
-function Project(props) {
-  if (props.link) {
-    return (
-      <a href={props.link}>
-        <ProjectStyled>
-          <p className="owner">{props.owner}</p>
-          <p className="name">{props.name}</p>
-          <p className="desc">{props.desc}</p>
-          <div className="links">
-            {props.codepenLink && (
-              <a
-                className="codepen"
-                href={props.codepenLink}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img className="icon codepen" src={CodePen} alt="CodePen" />
-              </a>
-            )}
-            <a
-              className="github"
-              href={props.githubLink}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img className="icon github" src={Github} alt="Github" />
-            </a>
-          </div>
-        </ProjectStyled>
-      </a>
-    );
-  } else {
-    return (
-      <ProjectStyled>
-        <p className="owner">{props.owner}</p>
-        <p className="name">{props.name}</p>
-        <p className="desc">{props.desc}</p>
-        <div className="links">
-          {props.codepenLink && (
-            <a
-              className="codepen"
-              href={props.codepenLink}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img className="icon codepen" src={CodePen} alt="CodePen" />
-            </a>
-          )}
+function Project({
+  owner,
+  name,
+  desc,
+  liveLink,
+  codepenLink,
+  githubLink,
+  githubProjectLink,
+}) {
+  return (
+    <ProjectStyled>
+      <p className="owner">
+        <a href={githubLink} target="_blank" rel="noopener noreferrer">
+          {owner}
+        </a>
+      </p>
+      <p className="name">{name}</p>
+      <p className="desc">{desc}</p>
+      <div className="links">
+        {!liveLink && codepenLink && (
           <a
-            className="github"
-            href={props.githubLink}
+            className="codepen"
+            href={codepenLink}
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img className="icon github" src={Github} alt="Github" />
+            <img className="icon codepen" src={CodePen} alt="CodePen" />
           </a>
-        </div>
-      </ProjectStyled>
-    );
-  }
+        )}
+        <a
+          className="github"
+          href={githubProjectLink}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img className="icon github" src={Github} alt="Github" />
+        </a>
+      </div>
+    </ProjectStyled>
+  );
 }
 
 export default Project;
