6720737e7131512761fc92f9fb8f5254c5339f07
Ryan Fuhrman <rsfuhrman10@gmail.com>
2019-06-05 13:42:10 -0400

    removed alternate render

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,41 +1,17 @@
 import React from 'react';
-import { Link } from 'gatsby';
 
 import StyledSection from './styles/SectionStyles';
 
-const Section = ({
-  className,
-  link,
-  iconLink,
-  header,
-  subheader,
-  children,
-}) => {
-  if (link) {
-    return (
-      <StyledSection className={className}>
-        <div className="header-div">
-          <h2 className="header">
-            <Link to={`/${link}`} className="link">
-              {header}
-            </Link>
-          </h2>
-        </div>
-        <h3 className="subheader">{subheader}</h3>
-        <div className="children">{children}</div>
-      </StyledSection>
-    );
-  } else {
-    return (
-      <StyledSection className={className}>
-        <div className="header-div">
-          <h2 className="header">{header}</h2>
-        </div>
-        <h3 className="subheader">{subheader}</h3>
-        <div className="children">{children}</div>
-      </StyledSection>
-    );
-  }
+const Section = ({ className, header, subheader, children }) => {
+  return (
+    <StyledSection className={className}>
+      <div className="header-div">
+        <h2 className="header">{header}</h2>
+      </div>
+      <h3 className="subheader">{subheader}</h3>
+      <div className="children">{children}</div>
+    </StyledSection>
+  );
 };
 
 export default Section;
