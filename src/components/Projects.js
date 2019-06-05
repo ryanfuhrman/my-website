39406cb250f22a40c47d7c30ef461a8515c75374
Ryan Fuhrman <rsfuhrman10@gmail.com>
2019-06-05 13:43:29 -0400

    Removed link to github on each card and replaced with button at bottom of component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,12 +13,12 @@ function Projects() {
     >
       <div className="project-container">
         <Project
-          className="twitch-app project"
+          className="twitch-browser project"
           owner="ryanfuhrman"
-          name="twitch-app"
+          name="twitch-browser"
           desc="An app to play with the Twitch API"
-          link="https://twitch-browser.ryanfuhrman.me"
-          githubLink="https://github.com/ryanfuhrman/twitch-browser"
+          liveLink="https://twitch-browser.ryanfuhrman.me"
+          githubProjectLink="https://github.com/ryanfuhrman/twitch-browser"
           codepenLink=""
         />
         <Project
@@ -26,7 +26,7 @@ function Projects() {
           owner="ryanfuhrman"
           name="markdown-viewer"
           desc="A markdown playground built with React"
-          githubLink="https://github.com/ryanfuhrman/markdown-viewer"
+          githubProjectLink="https://github.com/ryanfuhrman/markdown-viewer"
           codepenLink=""
         />
         <Project
@@ -34,7 +34,7 @@ function Projects() {
           owner="ryanfuhrman"
           name="my-website"
           desc="Hey, that's this website! 🚀"
-          githubLink="https://github.com/ryanfuhrman/my-website"
+          githubProjectLink="https://github.com/ryanfuhrman/my-website"
           codepenLink=""
         />
         <Project
@@ -42,10 +42,21 @@ function Projects() {
           owner="ryanfuhrman"
           name="calculator"
           desc="A javascript calculator"
-          githubLink="https://github.com/ryanfuhrman/calculator"
+          githubProjectLink="https://github.com/ryanfuhrman/calculator"
           codepenLink="https://codepen.io/ryanfuhrman/full/jZzooK"
         />
       </div>
+      <button className="more-btn">
+        <a
+          href="https://github.com/ryanfuhrman"
+          className="link"
+          target="_blank"
+          without
+          rel="noopener noreferrer"
+        >
+          Check Out the Rest
+        </a>
+      </button>
     </ProjectsStyled>
   );
 }
